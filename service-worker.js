f48eefc4364c218e1a835d1c9151889e7871a4dc
crujiff <crujiff.14@gmail.com>
2025-09-02 18:09:33 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,24 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("expense-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
+const CACHE_NAME = "spese-cache-v1";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
